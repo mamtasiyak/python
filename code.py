740ef4e176eae4d9329214adc21564a143bdec15
root <root@ip-172-31-8-31.ap-south-1.compute.internal>
2019-05-14 12:31:46 +0000

    first commit

---
+++ code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/pythonn3
+options='''press 1 TO check your Ec2 instance RAM
+press 2 To check since how long ec2 instance is running
+press 3 to check a command named tree and install it
+press 4 to check current time and date
+press 5 to login in your facebook account and update status hello world
+press 6 to search something on google
+press 7 to list all the IP & MAC in your current wifi'''
+print(options)
+print("@@@@@@@@@@@@@@@@@")
+print("@@             @@@@@@@@@@@@@")
+#capturing user
+choice=input()
+print(choice)
